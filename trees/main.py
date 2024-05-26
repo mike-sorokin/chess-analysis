0b0790b9ad1738981f673c26f5b789cdcc14ef30
Mike Sorokin <mike-sorokin@outlook.com>
2024-05-26 20:02:12 +0100

    Progress on trees

--- trees/main.py
+++ trees/main.py
@@ -1,6 +1,59 @@
 import chess
 import chess.engine
 
+class ChessTreeNode:
+    def __init__(self, node, children):
+        fen, move = node
+        board = chess.Board(fen)
+        board.push(move)
+        src, dst = move.from_square, move.to_square
+        self.move_attributes = {
+            "piece": board.piece_at(dst).symbol().upper(),
+            "from_file": chess.square_file(src),
+            "from_rank": chess.square_rank(src),
+            "from_diagonal_1": chess.square_file(src) + chess.square_rank(src),
+            "from_diagonal_2": chess.square_file(src) + (8 - chess.square_rank(src)),
+            "to_file": chess.square_file(dst),
+            "to_rank": chess.square_rank(dst),
+            "from_diagonal_1": chess.square_file(dst) + chess.square_rank(dst),
+            "from_diagonal_2": chess.square_file(dst) + (8 - chess.square_rank(dst)),
+            "move_distance": chess.square_distance(src, dst),
+            "check": board.is_check(),
+            "mate": board.is_checkmate(),
+            "num_attacks": sum(1 for _, p 
+                in board.piece_map(mask=int(board.attacks(dst))).items()
+                if p.color != board.turn
+            ),
+            "num_defenses": sum(1 for _, p 
+                in board.piece_map(mask=int(board.attacks(dst))).items()
+                if p.color == board.turn
+            ),
+            "attacked_by_num": len(board.attackers(not board.turn, dst)),
+            "defended_by_num": len(board.attackers(board.turn, dst)),
+        }
+        self.children = [ChessTreeNode(*c) for c in children]
+        board.pop()
+        self.board = board
+        self.mv = move
+
+    def _print(self, n=0):
+        print("    "*n + self.board.san(self.mv) + str(self.move_attributes))
+        print()
+        for c in self.children:
+            c._print(n+1)
+
+    @staticmethod
+    def get_label(node):
+        return node.move_attributes
+    
+    @staticmethod
+    def get_children(node):
+        return node.children
+    
+    @staticmethod
+    def compare(a, b):
+        pass
+
 def expand_tree(fen, move, engine, can_terminate_on_equal_moves, eval_threshold=100, depth=0):
     print(fen, move)
     board = chess.Board(fen)
@@ -54,8 +107,8 @@ def main():
 
     stockfish = chess.engine.SimpleEngine.popen_uci("stockfish-ubuntu-x86-64-avx2/stockfish/stockfish-ubuntu-x86-64-avx2")
     tree = expand_tree(simple, chess.Move.null(), stockfish, True)
+    ChessTreeNode(*tree[0])._print()
     stockfish.quit()
-    print_tree(tree)
 
 if __name__ == "__main__":
     main()
